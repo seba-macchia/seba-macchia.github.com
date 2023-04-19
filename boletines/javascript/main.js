5d7fefdbc9d558dd08111de009cfc7f0b60eecb4
seba-macchia <macchiasebastianprog@gmail.com>
2023-04-19 19:53:36 -0300

    Redirecciona a /boletines

--- boletines/javascript/main.js
+++ boletines/javascript/main.js
@@ -12,7 +12,27 @@ console.log(tabla);
 let link = document.querySelectorAll(".close");
 
 // Recorrerlos
-links.forEach(function(link){
+  links.forEach(function(link){
     // Agregar un evento click a cada uno de ellos - case sensitive
     link.addEventListener("click", function(){})
-});
+    eval.preventDefault();
+
+    // Quitarle las clases de animacion que ya tiene
+    content.classList.remove("animate__fadeInUpBig");
+    content.classList.remove("animate__flash");
+
+    // Agregar clases para animar su salida faceOutUp
+    content.classList.add("animate__fadeInUpBig");
+    content.classList.add("animate__flash");
+
+    setTimeout(function(){
+      location.href ="/boletines";
+    },600);
+
+
+    return false;
+
+    });
+  
+
+
