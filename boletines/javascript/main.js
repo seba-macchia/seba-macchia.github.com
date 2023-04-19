229e8b68dcdb651fbfb5abb4c8178c413af17075
seba-macchia <macchiasebastianprog@gmail.com>
2023-04-19 19:39:41 -0300

    Agregamos el proyecto boletines

---
+++ boletines/javascript/main.js
@@ -0,0 +1,18 @@
+console.log("Hola mundo desde javaScript")
+
+//DOM
+// querySelector / querySelectorAll
+/*
+let tabla = document.querySelector("table");
+console.log(tabla);
+*/
+
+
+// Obtener los elementos de la clase .close
+let link = document.querySelectorAll(".close");
+
+// Recorrerlos
+links.forEach(function(link){
+    // Agregar un evento click a cada uno de ellos - case sensitive
+    link.addEventListener("click", function(){})
+});
